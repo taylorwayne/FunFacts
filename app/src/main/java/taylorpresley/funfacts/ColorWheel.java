d30c48619ccfa64e9947a432aad0613d44b89c16
taylorwayne <taylor.presley@missiondrivenresearch.com>
2016-07-09 18:39:01 -0500

    Third Commit

---
+++ app/src/main/java/taylorpresley/funfacts/ColorWheel.java
@@ -0,0 +1,38 @@
+package taylorpresley.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by Taylor Presley on 05-Jun-16.
+ */
+public class ColorWheel{
+    // Fields (Member Variables) - Propertis about the object
+    private final String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"};  // light gray
+    // Methods - Actions the object can take
+    public int getColor(){
+        // The button was clicked, so update the factTextView with a new fact
+        String color;
+        // Randomly select a fact
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        color = mColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    };
+}
